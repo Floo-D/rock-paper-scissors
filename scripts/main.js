bf1566d2132c3333c3bb25ff0bc0f4f6315aa6d5
Florian Dauw <dauw.florian@hotmail.com>
2019-12-13 03:48:47 +0100

    Delete useless function

--- scripts/main.js
+++ scripts/main.js
@@ -3,35 +3,6 @@ function computerPlay() {
     return action[Math.floor(Math.random() * 3)];
 }
 
-/*function playRound(playerSelection, computerSelection) {
-    const choice = playerSelection.toLowerCase();
-    if(choice === "rock") {
-        if(computerSelection === "paper") {
-            console.log("You Lose! Paper beats Rock");
-        } if(computerSelection === "scissors") {
-            console.log("You Win! Rock beats Scissors");
-        } else {
-            console.log("It's a Draw!...");
-        }
-    } else if(choice === "paper") {
-        if(computerSelection === "scissors") {
-            console.log("You Lose! Scissors beats Paper");
-        } else if(computerSelection === "rock"){
-            console.log("You Win! Paper beats Scissors");
-        } else {
-            console.log("It's a Draw!...");
-        }
-    } else {
-        if(computerSelection === "rock") {
-            console.log("You Lose! Rock beats Scissors");
-        } else if(computerSelection === "paper") {
-            console.log("You Win! Scissors beats paper");
-        } else {
-            console.log("It's a Draw!...");
-        }
-    }
-} */
-
 function playRound(playerSelection, computerSelection) {
     const choice = playerSelection.toLowerCase();
     if(choice === computerSelection) {
